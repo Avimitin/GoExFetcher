6171d457bf4fa235711c32c942ab61029ee1f7ba
avimitin <avimitin@gmail.com>
2020-12-07 17:37:32 +0800

    Add test to fit github workflow

--- internal/browser/APIHelper_test.go
+++ internal/browser/APIHelper_test.go
@@ -3,12 +3,20 @@ package browser
 import "testing"
 
 func TestReg(t *testing.T) {
-	a := Reg("https://exhentai.org/g/")
+	a := Reg("https://exhentai.org/g/1761748/882d6987a6")
 	if a == "" {
-		t.Fail()
+		t.Errorf("Expect 1761748/882d6987a6 but got nothing")
+		t.FailNow()
+	}
+	if a != "1761748/882d6987a6" {
+		t.Errorf("Expect 1761748/882d6987a6 but got %s", a)
+		t.Failed()
 	}
 }
 
 func TestGetGIDAndToken(t *testing.T) {
-	GetGIDAndToken("1761748/882d6987a6")
+	result := GetGIDAndToken("1761748/882d6987a6")
+	if result == nil || result[0] != "1761748" || result[1] != "882d6987a6" {
+		t.Errorf("Expect result[0] == 1761748, result[1] == 882d6987a6, but got %v", result)
+	}
 }
--- internal/browser/browser_test.go
+++ internal/browser/browser_test.go
@@ -5,5 +5,16 @@ import (
 )
 
 func TestJsonBrowse(t *testing.T) {
-	Browser(1761748, "882d6987a6")
+	result, err := Browser("1761748", "882d6987a6")
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	if result == nil {
+		t.Errorf("Fetch unexpected nil struct")
+		t.FailNow()
+	}
+	if len(result.Gmd) == 0 {
+		t.Errorf("Can't fetch result")
+	}
 }
