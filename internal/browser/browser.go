610a292dada420218ca9dc6f97aee84f26e19cde
avimitin <avimitin@gmail.com>
2021-01-03 12:05:04 +0800

    Add more error message

--- internal/browser/browser.go
+++ internal/browser/browser.go
@@ -3,47 +3,66 @@ package browser
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 // JSONBrowse is only for json browse
-func parser(body []byte) *G {
-	var repJSON *G
+func parse(body []byte) (*G, error) {
+	var repJSON G
 	err := json.Unmarshal(body, &repJSON)
-	CheckErr(err)
-	return repJSON
+	if err != nil {
+		return nil, fmt.Errorf("unmarshall %s: %v", body, err)
+	}
+	return &repJSON, nil
 }
 
 // Browser use given gallery id and token to fetch comic info
 // return a gmetadata struct pointer.
 func Browser(galleryID string, token string) (*G, error) {
-	gd, err := NewGData(galleryID, token)
-	if gd == nil {
-		return nil, &InvalidKeyError{
-			Gid:       galleryID,
-			ErrorInfo: err.Error(),
-		}
+	gdata := NewGData(0)
+	gdata.SetComic(galleryID, token)
+	gd, err := json.Marshal(&gdata)
+	if err != nil {
+		return nil, fmt.Errorf("Browser: marshal %+v: %v", gdata, err)
 	}
 
 	req, err := http.NewRequest("POST", "https://api.e-hentai.org/api.php", bytes.NewReader(gd))
 	// post request.
-	CheckErr(err)
+	if err != nil {
+		return nil, fmt.Errorf("Browser: new request %v", err)
+	}
 
 	client := NewHTTPClient()
 	resp, err := client.Do(req)
-	CheckErr(err)
+	if err != nil {
+		return nil, fmt.Errorf("Browser: do request %v got %v", req, err)
+	}
 
 	// read and store bytes.
 	body, err := ioutil.ReadAll(resp.Body)
-	CheckErr(err)
+	if err != nil {
+		return nil, fmt.Errorf("Browser: read response %v", err)
+	}
 
-	g := parser(body)
+	g, err := parse(body)
+	if err != nil {
+		return nil, fmt.Errorf("Browser: parse %v got %w", body, err)
+	}
+	var gmdWithoutErr []GMetaData
 	for _, gmd := range g.Gmd {
 		if gmd.Err != "" {
 			log.Printf("[Input Error]%d's token is invalid or this gid is not existed.", gmd.Gid)
+			continue
 		}
+		gmdWithoutErr = append(gmdWithoutErr, gmd)
 	}
+	if len(gmdWithoutErr) == 0 {
+		return nil, errors.New("Browser: all given comic are invalid")
+	}
+	g.Gmd = gmdWithoutErr
 	return g, nil
 }
